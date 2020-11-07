f21035812afebbd4355faa3ed0166a5a1476f4d0
artur.wozniak <wozniaka83@gmail.com>
2020-11-07 10:31:35 +0100

    app: https

--- src/App.js
+++ src/App.js
@@ -81,8 +81,9 @@ const getFilmsData = async (films, dispatch) => {
   const response = [];
 
   films.forEach(async (personFilm, index) => {
+    const personFilmHTTPS = personFilm.replace('http', 'https');
     try {
-      const responseData = await fetch(personFilm);
+      const responseData = await fetch(personFilmHTTPS);
       const personFilmData = await responseData.json();
       response[index] = personFilmData;
       dispatch({ type: types.SET_FILMS, films: response });
